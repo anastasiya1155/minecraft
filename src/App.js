18fa56f7f889664a42e9842a91e5aa181637f3d2
anastasiia <anastasiya1155@gmail.com>
2022-10-09 11:38:39 +0700

    camera movements

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import { Sky } from "@react-three/drei";
 import { Physics } from "@react-three/cannon";
 import { Ground } from "./components/Ground";
 import { Player } from "./components/Player";
+import { FPV } from "./components/FPV";
 
 function App() {
   return (
-    <Canvas>
-      <Sky sunPosition={[100, 100, 20]} />
-      <ambientLight intensity={0.5} />
-      <Physics>
-        <Player/>
-        <Ground/>
-      </Physics>
-    </Canvas>
+    <>
+      <Canvas>
+        <Sky sunPosition={[100, 100, 20]} />
+        <ambientLight intensity={0.5} />
+        <FPV/>
+        <Physics>
+          <Player/>
+          <Ground/>
+        </Physics>
+      </Canvas>
+      <div className="absolute cursor centered">+</div>
+    </>
   );
 }
 
