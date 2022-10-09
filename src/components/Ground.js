74fd36264b258a3abc141797883694cf9f169f0f
anastasiia <anastasiya1155@gmail.com>
2022-10-09 11:03:51 +0700

    add player component

--- src/components/Ground.js
+++ src/components/Ground.js
@@ -4,7 +4,7 @@ import { NearestFilter, RepeatWrapping } from "three";
 
 export const Ground = () => {
   const [ref] = usePlane(() => ({
-    rotation: [-Math.PI / 4,0,0],
+    rotation: [-Math.PI / 2,0,0],
     position: [0,0,0]
   }));
 
@@ -15,7 +15,7 @@ export const Ground = () => {
 
   return (
     <mesh ref={ref}>
-      <planeBufferGeometry attach="geometry" args={[100,100]} />
+      <planeGeometry attach="geometry" args={[100,100]} />
       <meshStandardMaterial attach="material" map={groundTexture} />
     </mesh>
   )
